aa6ac4113e4e9d1c9554b92c47aafbac94742ca9
victorli5674 <victorli5674@yahoo.com>
2017-02-08 12:01:07 -0800

    started using a text file to execute statments

---
+++ CS157BProject1/bin/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Sale_Transaction (Date, Spent, NumberOfItems) VALUES ("March 5", 6.99, 5);
+
+INSERT INTO Sale_Transaction (Date, Spent, NumberOfItems) VALUES ("March 7", 11.99, 11);
--- CS157BProject1/src/Database.java
+++ CS157BProject1/src/Database.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.io.*;
+import java.sql.*;
 import java.util.Properties;
 
 
@@ -21,11 +20,10 @@ public class Database {
 				System.out.println("Creating database SALES_TRANSACTIONS");
 				stmt = conn.createStatement();
 				
-				//drop database sales_transactions if it is already exists
-				//comment out if it doesn't exist
-				stmt.execute("DROP DATABASE SALES_TRANSACTIONS");
+				//create database sales_transactions if it is does not exist
+				stmt.execute("CREATE DATABASE IF NOT EXISTS Sales_Transactions");
 				
-				stmt.execute("CREATE DATABASE SALES_TRANSACTIONS");
+				stmt.close();
 				System.out.println("Database created successfully");
 			} catch(Exception e) {
 				e.printStackTrace();
@@ -51,13 +49,25 @@ public class Database {
 			
 			try {
 				stmt = conn.createStatement();
+				//drop table sale_transaction if it exists
+				stmt.execute("DROP TABLE IF EXISTS Sale_Transaction");
 				
 				stmt.executeUpdate("CREATE TABLE Sale_Transaction " +
 						"(CustomerID INTEGER NOT NULL AUTO_INCREMENT, " +
 						"Spent FLOAT, " +
-						"DATE VARCHAR(255), " +
+						"Date VARCHAR(255), " +
 						"NumberOfItems INTEGER NOT NULL, " +
 						"PRIMARY KEY (CustomerID))");
+				
+				//change destination if needed
+				FileReader file = new FileReader("C:/school/CS157BProject1/CS157BProject1/src/insert.sql");
+				BufferedReader br = new BufferedReader(file);
+				
+				String query;
+				while((query = br.readLine()) != null) {
+					if(!query.trim().equals(""))
+						stmt.executeUpdate(query);
+				}
 				stmt.close();
 				System.out.println("Created Sales_Transaction table");
 			} catch(Exception e) {
@@ -65,7 +75,6 @@ public class Database {
 			} finally {
 				conn.close();
 			}
-			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
