63da96b522e3c6820c9510c6267b25304246b106
victorli5674 <victorli5674@yahoo.com>
2017-02-09 23:57:08 -0800

    deleted most of code, so queries can run from text file

--- CS157BProject1/src/Database.java
+++ CS157BProject1/src/Database.java
@@ -8,46 +8,13 @@ public class Database {
 	//set up database sales_transactions
 	public static void setupDB(Properties info, String connString,
 			String jdbcDriver) {
+		
 		Connection conn;
 		Statement stmt;
 		
 		try {
 			Class.forName(jdbcDriver);
-			
 			conn = DriverManager.getConnection(connString, info);
-			try {
-				System.out.println("Creating database SALES_TRANSACTIONS");
-				stmt = conn.createStatement();
-				
-				//create database sales_transactions if it is does not exist
-				stmt.execute("CREATE DATABASE IF NOT EXISTS Sales_Transactions");
-				
-				stmt.close();
-				System.out.println("Database created successfully");
-			} catch(Exception e) {
-				e.printStackTrace();
-			} finally {
-				conn.close(); //close connection
-			}
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		//reconnect with Sales_Transaction database
-		String SalesTransactionConnection = "jdbc:mysql://localhost/SALES_TRANSACTIONS";
-		populateDB(info, SalesTransactionConnection, jdbcDriver);
-		
-	}
-
-	private static void populateDB(Properties info,
-			String salesTransactionConnection, String jdbcDriver) {
-		Connection conn;
-		Statement stmt;
-		
-		try {
-			Class.forName(jdbcDriver);
-			conn = DriverManager.getConnection(salesTransactionConnection, info);
 			
 			try {
 				stmt = conn.createStatement();
@@ -62,7 +29,7 @@ public class Database {
 						stmt.executeUpdate(query);
 				}
 				stmt.close();
-				System.out.println("Created Sales_Transaction table");
+				System.out.println("Created school database");
 			} catch(Exception e) {
 				e.printStackTrace();
 			} finally {
